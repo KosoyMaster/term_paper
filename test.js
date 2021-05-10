64625268bf4bb03fe1951b657cd4b9fe1f0f39ae
KosoyMaster <78322449+KosoyMaster@users.noreply.github.com>
2021-05-10 17:05:46 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,26 @@
+
+
+
+
+let mall = ['Январь', 'Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь']
+for (let i = 0; i < mall.length; i++) {
+    let winter = [];
+    if (mall[i] == 'Январь') {
+        winter.splice(0,0,mall[i]);
+        console.log(winter);
+    }
+    if
+        (mall[i] == 'Февраль') {
+            winter.splice(0,0,mall[i]);
+            console.log(winter);
+        }
+
+}
+
+// console.log(mall)
+// console.log('---------------------------')
+// console.log('Зима -----',mall[i]);
+// console.log('Весна -----',)
+// console.log('Лето -----',)
+// console.log('Осень -----',)
+
